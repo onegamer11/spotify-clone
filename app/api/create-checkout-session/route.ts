84006dae8761d19bed9691e4ba72d680b633fef9
onegamer11 <70062981+onegamer11@users.noreply.github.com>
2024-01-10 15:29:48 +0530

    Update route.ts

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -36,7 +36,7 @@ export async function POST(
       mode: 'subscription',
       allow_promotion_codes: true,
       subscription_data: {
-        trial_from_plan: true,
+        
         metadata
       },
       success_url: `${getURL()}/account`,
